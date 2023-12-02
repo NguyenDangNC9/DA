84d12111c3abfbdc029cfe6af476d6dd3453d7f6
= <117330765+yen-nuong@users.noreply.github.com>
2023-12-02 20:18:38 +0700

    cập nhật giá

--- src/main/java/com/poly/controller/admin/HomeAdminController.java
+++ src/main/java/com/poly/controller/admin/HomeAdminController.java
@@ -35,9 +35,8 @@ import com.poly.entity.ReportProductbyDay;
 import com.poly.service.AccountService;
 import com.poly.service.ReportService;
 
-
 @Controller
-public class HomeAdminController{
+public class HomeAdminController {
 	@Autowired
 	ProductDao pdao;
 	@Autowired
@@ -53,12 +52,12 @@ public class HomeAdminController{
 	@Autowired
 	ReportProductbyDayDao pReportProductbyDayDao;
 	@Autowired
-//	PostDao postDao;
-	
-	
+	// PostDao postDao;
+
 	AccountDao dao;
+
 	@RequestMapping("/admin/home/index")
-	public String home(Model model, @RequestParam("page") Optional<Integer> page ) { 
+	public String home(Model model, @RequestParam("page") Optional<Integer> page) {
 		model.addAttribute("order", odao.getTop10());
 		model.addAttribute("protop10", pReportProductbyDayDao.reportProdctTop10());
 		int x = page.orElse(0);
@@ -68,152 +67,157 @@ public class HomeAdminController{
 		model.addAttribute("prosize", pdao.findAll().size());
 		model.addAttribute("accsize", adao.findAll().size());
 		model.addAttribute("ordersize", odao.findAll().size());
-//		model.addAttribute("postsize", postDao.findAll().size());
+		// model.addAttribute("postsize", postDao.findAll().size());
 		return "admin/layout/home";
 	}
+
 	@RequestMapping("/admin/look")
-	public String look() { 
+	public String look() {
 		return "admin/security/look";
 	}
+
 	@RequestMapping("/admin/authority")
-	public String authority() { 
+	public String authority() {
 		return "admin/authority/authority";
 	}
-		@RequestMapping("/admin/map")
-		public String map() { 
-			return "admin/layout/map";
-		}
-		@RequestMapping("/admin/char/charpro")
-		public String charpro(Model model) { 
-		
-			model.addAttribute("items", reportdao.getReportProductbyDaynoMinMax());
-			return "admin/char/charpro";
-		}
-		@RequestMapping("/admin/char/charuser")
-		public String charuser(Model model ,@RequestParam(value= "minday" , required = false)
-		@DateTimeFormat(pattern="yyyy-MM-dd") Date minday,
-	    		@RequestParam(value = "maxday", required = false)
-		@DateTimeFormat(pattern="yyyy-MM-dd") Date maxday) {
-			 try {
-			        
-	    		  Map<String,Double> barChartData = new TreeMap<>();
-	    		  Map<String,Long> barChartData1 = new TreeMap<>();
-	    		  
-	    	    	if(minday==(null)  && maxday==(null)) {
-	    	    		List<Report>  report =  rDao.revenueByCustomer();
-	    	    		model.addAttribute("items", report);
-	    	            for(int i=0 ; i<report.size();i++) {
-	    	            	barChartData.put(report.get(i).getGroup().toString(),report.get(i).getSum());
-	    	            
-	    	            }
-	    	            for(int i=0 ; i<report.size();i++) {
-	    	            	barChartData1.put(report.get(i).getGroup().toString(),report.get(i).getCount());
-	    	            
-	    	            }
-	    	    	}
-	    	    	else {
-	    	    	List<Report>  report =  rDao.revenueByCustomerBydate(minday,maxday);
-	    	    	model.addAttribute("items", report);
-	    	        for(int i=0 ; i<report.size();i++) {
-	    	        	barChartData.put(report.get(i).getGroup().toString(),report.get(i).getSum());
-	    	        }
-	    	        for(int i=0 ; i<report.size();i++) {
-   	            	barChartData1.put(report.get(i).getGroup().toString(),report.get(i).getCount());
-   	            
-   	            }
-	    	    	}
-	    	    	
-	    	        model.addAttribute("barChartData", barChartData);
-	    	        model.addAttribute("barChartData1", barChartData1);
-	    	        model.addAttribute("size", barChartData.size());
-	    	        model.addAttribute("message", "Thống kê thành công");
-	    	        return "admin/char/charuser";
-				
-			} catch (Exception e) {
-				model.addAttribute("message", "Không được để trống Minday hoặc Maxday");
-				return "admin/char/charuser";
-			} 
-			
-		}
-		@RequestMapping("/admin/char/charday")
-		public String charday(Model model) { 
-			model.addAttribute("items", rDao.revenueByMonth());
-			return "admin/char/charday";
-		}
-		@RequestMapping("/admin/char/charcate")
-		public String charcate(Model model) { 
-			model.addAttribute("items", rDao.revenueByCategory());
-			return "admin/char/charcate";
-		}
-		@RequestMapping("/admin/char/chartrak")
-		public String chartrak(Model model) { 
-			model.addAttribute("items", rDao.revenueByTrak());
-			return "admin/char/chartrak";
-		}
-		@RequestMapping("/admin/char/charyear")
-		public String charyear(Model model, @RequestParam("year") int year) { 
-			 Map<String,Double> barChartData = new TreeMap<>();
-   		  Map<String,Long> barChartData1 = new TreeMap<>();
-   		List<Report> list = rDao.revenueByyear(year);
-   		for(int i=0 ; i<list.size();i++) {
-        	barChartData.put(list.get(i).getGroup().toString(),list.get(i).getSum());
-        }
-        for(int i=0 ; i<list.size();i++) {
-        	barChartData1.put(list.get(i).getGroup().toString(),list.get(i).getCount());
-        
-        }
-        model.addAttribute("barChartData", barChartData);
-        model.addAttribute("barChartData1", barChartData1);
-			
-			
-			model.addAttribute("items", list);
-			return "admin/char/charyear";
-		}
-		@RequestMapping("/admin/char/product")
-		    public String home(Model model ,@RequestParam(value= "minday" , required = false)@DateTimeFormat(pattern="yyyy-MM-dd") Date minday,
-		    		@RequestParam(value = "maxday", required = false)@DateTimeFormat(pattern="yyyy-MM-dd") Date maxday){
 
-		        try {
-		        
-		    		  Map<String,Double> barChartData = new TreeMap<>();
-		    		  Map<String,Long> barChartData1 = new TreeMap<>();
-		    		  
-		    	    	if(minday==(null)  && maxday==(null)) {
-		    	    		List<ReportProductbyDay>  report =  pReportProductbyDayDao.reportProdctByDaynoMinMax();
-		    	    		model.addAttribute("listreprt", report);
-		    	            for(int i=0 ; i<report.size();i++) {
-		    	            	barChartData.put(report.get(i).getName().toString(),report.get(i).getSum());
-		    	            
-		    	            }
-		    	            for(int i=0 ; i<report.size();i++) {
-		    	            	barChartData1.put(report.get(i).getName().toString(),report.get(i).getCount());
-		    	            
-		    	            }
-		    	    	}
-		    	    	else {
-		    	    	List<ReportProductbyDay>  report =  pReportProductbyDayDao.reportProdctByDay(minday,maxday);
-		    	    	model.addAttribute("listreprt", report);
-		    	        for(int i=0 ; i<report.size();i++) {
-		    	        	barChartData.put(report.get(i).getName().toString(),report.get(i).getSum());
-		    	        }
-		    	        for(int i=0 ; i<report.size();i++) {
-	    	            	barChartData1.put(report.get(i).getName().toString(),report.get(i).getCount());
-	    	            
-	    	            }
-		    	    	}
-		    	    	
-		    	        model.addAttribute("barChartData", barChartData);
-		    	        model.addAttribute("barChartData1", barChartData1);
-		    	        model.addAttribute("size", barChartData.size());
-		    	        model.addAttribute("message", "Thống kê thành công");
-		    	        return "admin/char/chartest";
-					
-				} catch (Exception e) {
-					model.addAttribute("message", "Không được để trống Minday hoặc Maxday");
-					  return "admin/char/chartest";
-				} 
+	@RequestMapping("/admin/map")
+	public String map() {
+		return "admin/layout/map";
+	}
+
+	@RequestMapping("/admin/char/charpro")
+	public String charpro(Model model) {
+
+		model.addAttribute("items", reportdao.getReportProductbyDaynoMinMax());
+		return "admin/char/charpro";
+	}
+
+	@RequestMapping("/admin/char/charuser")
+	public String charuser(Model model,
+			@RequestParam(value = "minday", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date minday,
+			@RequestParam(value = "maxday", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date maxday) {
+		try {
+
+			Map<String, Double> barChartData = new TreeMap<>();
+			Map<String, Long> barChartData1 = new TreeMap<>();
+
+			if (minday == (null) && maxday == (null)) {
+				List<Report> report = rDao.revenueByCustomer();
+				model.addAttribute("items", report);
+				for (int i = 0; i < report.size(); i++) {
+					barChartData.put(report.get(i).getGroup().toString(), report.get(i).getSum());
+
+				}
+				for (int i = 0; i < report.size(); i++) {
+					barChartData1.put(report.get(i).getGroup().toString(), report.get(i).getCount());
+
+				}
+			} else {
+				List<Report> report = rDao.revenueByCustomerBydate(minday, maxday);
+				model.addAttribute("items", report);
+				for (int i = 0; i < report.size(); i++) {
+					barChartData.put(report.get(i).getGroup().toString(), report.get(i).getSum());
+				}
+				for (int i = 0; i < report.size(); i++) {
+					barChartData1.put(report.get(i).getGroup().toString(), report.get(i).getCount());
+
+				}
+			}
+
+			model.addAttribute("barChartData", barChartData);
+			model.addAttribute("barChartData1", barChartData1);
+			model.addAttribute("size", barChartData.size());
+			model.addAttribute("message", "Thống kê thành công");
+			return "admin/char/charuser";
+
+		} catch (Exception e) {
+			model.addAttribute("message", "Không được để trống Minday hoặc Maxday");
+			return "admin/char/charuser";
 		}
-	
-	
-	
+
+	}
+
+	@RequestMapping("/admin/char/charday")
+	public String charday(Model model) {
+		model.addAttribute("items", rDao.revenueByMonth());
+		return "admin/char/charday";
+	}
+
+	@RequestMapping("/admin/char/charcate")
+	public String charcate(Model model) {
+		model.addAttribute("items", rDao.revenueByCategory());
+		return "admin/char/charcate";
+	}
+
+	@RequestMapping("/admin/char/chartrak")
+	public String chartrak(Model model) {
+		model.addAttribute("items", rDao.revenueByTrak());
+		return "admin/char/chartrak";
+	}
+
+	@RequestMapping("/admin/char/charyear")
+	public String charyear(Model model, @RequestParam("year") int year) {
+		Map<String, Double> barChartData = new TreeMap<>();
+		Map<String, Long> barChartData1 = new TreeMap<>();
+		List<Report> list = rDao.revenueByyear(year);
+		for (int i = 0; i < list.size(); i++) {
+			barChartData.put(list.get(i).getGroup().toString(), list.get(i).getSum());
+		}
+		for (int i = 0; i < list.size(); i++) {
+			barChartData1.put(list.get(i).getGroup().toString(), list.get(i).getCount());
+
+		}
+		model.addAttribute("barChartData", barChartData);
+		model.addAttribute("barChartData1", barChartData1);
+
+		model.addAttribute("items", list);
+		return "admin/char/charyear";
+	}
+
+	@RequestMapping("/admin/char/product")
+	public String home(Model model,
+			@RequestParam(value = "minday", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date minday,
+			@RequestParam(value = "maxday", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date maxday) {
+
+		try {
+
+			Map<String, Double> barChartData = new TreeMap<>();
+			Map<String, Long> barChartData1 = new TreeMap<>();
+
+			if (minday == (null) && maxday == (null)) {
+				List<ReportProductbyDay> report = pReportProductbyDayDao.reportProdctByDaynoMinMax();
+				model.addAttribute("listreprt", report);
+				for (int i = 0; i < report.size(); i++) {
+					barChartData.put(report.get(i).getName().toString(), report.get(i).getSum());
+
+				}
+				for (int i = 0; i < report.size(); i++) {
+					barChartData1.put(report.get(i).getName().toString(), report.get(i).getCount());
+
+				}
+			} else {
+				List<ReportProductbyDay> report = pReportProductbyDayDao.reportProdctByDay(minday, maxday);
+				model.addAttribute("listreprt", report);
+				for (int i = 0; i < report.size(); i++) {
+					barChartData.put(report.get(i).getName().toString(), report.get(i).getSum());
+				}
+				for (int i = 0; i < report.size(); i++) {
+					barChartData1.put(report.get(i).getName().toString(), report.get(i).getCount());
+
+				}
+			}
+
+			model.addAttribute("barChartData", barChartData);
+			model.addAttribute("barChartData1", barChartData1);
+			model.addAttribute("size", barChartData.size());
+			model.addAttribute("message", "Thống kê thành công");
+			return "admin/char/chartest";
+
+		} catch (Exception e) {
+			model.addAttribute("message", "Không được để trống Minday hoặc Maxday");
+			return "admin/char/chartest";
+		}
+	}
+
 }
